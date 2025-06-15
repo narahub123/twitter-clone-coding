1536747463df36c979bd333273f5a29893faa37b
nara <parknara0213@gmail.com>
2025-06-15 22:21:25 +0900

    chore: 스타일 초기화 설정
    Refs: #4

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import "@shared/styles/reset.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
