7d734106087c82f750895f03dda2f613188e3961
nara <parknara0213@gmail.com>
2025-06-15 20:20:33 +0900

    chore: 절대 경로 설정
    Refs: #2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@app", replacement: "/src/app" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@features", replacement: "/src/features" },
+      { find: "@entities", replacement: "/src/entities" },
+      { find: "@shared", replacement: "/src/shared" },
+      { find: "@widgets", replacement: "/src/widgets" },
+      { find: "@processes", replacement: "/src/processes" },
+    ],
+  },
+});
