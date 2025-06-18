155022f0a2a5de865f5f79aec9e8be9c5b9d80ca
nara <parknara0213@gmail.com>
2025-06-18 23:02:50 +0900

    feat: 토큰 디자인 시스템 도입
    Refs: #10

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,15 +1,40 @@
 import type { ReactNode } from "react";
 import styles from "./Button.module.css";
-import { joinClassNames } from "@shared";
+import tokens from "@shared/styles/design-system.module.css";
+import {
+  joinClassNames,
+  type ColorSchemeType,
+  type RoundedType,
+  type SizeType,
+  type VariantType,
+} from "@shared";
 
 interface ButtonProps {
   className?: string;
   disabled?: boolean;
   children: ReactNode;
+  size?: SizeType;
+  rounded?: RoundedType;
+  color?: ColorSchemeType;
+  variant?: VariantType;
 }
 
-const Button = ({ className, disabled = false, children }: ButtonProps) => {
-  const classNames = joinClassNames([styles["button"], className]);
+const Button = ({
+  className,
+  disabled = false,
+  size = "md",
+  rounded = "md",
+  color = "gray",
+  variant = "solid",
+  children,
+}: ButtonProps) => {
+  const classNames = joinClassNames([
+    styles["button"],
+    tokens[`button-size-${size}`],
+    tokens[`rounded-${rounded}`],
+    tokens[`${color}-${variant}`],
+    className,
+  ]);
 
   return (
     <button className={classNames} disabled={disabled} aria-disabled={disabled}>
