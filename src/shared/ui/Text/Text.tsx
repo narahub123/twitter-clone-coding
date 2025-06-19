e9d7e177324d6afa940209cbb5b54147614c0312
nara <parknara0213@gmail.com>
2025-06-19 21:51:47 +0900

    feat: fontSize, fontWeight, color에 토큰 디자인 시스템 도입
    Refs: #13

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -1,14 +1,35 @@
 import type { ReactNode } from "react";
 import styles from "./Text.module.css";
-import { joinClassNames } from "@shared";
+import tokens from "@shared/styles/design-system.module.css";
+import {
+  joinClassNames,
+  type ColorSchemeType,
+  type SizeType,
+  type WeightType,
+} from "@shared";
 
 interface TextProps {
   className?: string;
   children: ReactNode;
+  fontSize?: SizeType;
+  fontWeight?: WeightType;
+  color?: ColorSchemeType;
 }
 
-const Text = ({ className, children }: TextProps) => {
-  const classNames = joinClassNames([styles["text"], className]);
+const Text = ({
+  className,
+  children,
+  fontSize = "md",
+  fontWeight = "normal",
+  color = "gray",
+}: TextProps) => {
+  const classNames = joinClassNames([
+    styles["text"],
+    tokens[`text-font-size-${fontSize}`],
+    tokens[`font-weight-${fontWeight}`],
+    tokens[`text-${color}`],
+    className,
+  ]);
 
   return <p className={classNames}>{children}</p>;
 };
