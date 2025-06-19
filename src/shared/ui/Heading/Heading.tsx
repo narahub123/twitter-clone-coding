88a151fd3fb4da8815532b7afa0f120c424d1fc6
nara <parknara0213@gmail.com>
2025-06-19 23:39:27 +0900

    feat: as, color, weight 토큰 적용
    Refs: #14

--- src/shared/ui/Heading/Heading.tsx
+++ src/shared/ui/Heading/Heading.tsx
@@ -1,18 +1,48 @@
 import type { HTMLAttributes, ReactNode } from "react";
 import styles from "./Heading.module.css";
-import { joinClassNames } from "@shared";
+import tokens from "@shared/styles/design-system.module.css";
+import {
+  joinClassNames,
+  type ColorSchemeType,
+  type HeadingLevel,
+  type WeightType,
+} from "@shared";
 
 interface BaseProps {
   className?: string;
   children: ReactNode;
+  as?: HeadingLevel;
+  weight?: WeightType;
+  color?: ColorSchemeType;
 }
 
 type HeadingProps = BaseProps & HTMLAttributes<HTMLHeadingElement>;
 
-const Heading = ({ className, children }: HeadingProps) => {
-  const classNames = joinClassNames([styles["heading"], className]);
+const Heading = ({
+  className,
+  children,
+  as = "h1",
+  weight = "bold",
+  color = "gray",
+  ...rest
+}: HeadingProps) => {
+  const classNames = joinClassNames([
+    styles["heading"],
+    tokens[`heading-${as}`],
+    tokens[`font-weight-${weight}`],
+    tokens[`text-${color}`],
+    className,
+  ]);
 
-  return <h1 className={classNames}>{children}</h1>;
+  const Component = as;
+
+  const { style, ...headingRest } = rest;
+
+  return (
+    <Component className={classNames} style={style} {...headingRest}>
+      {children}
+    </Component>
+  );
 };
 
 export default Heading;
