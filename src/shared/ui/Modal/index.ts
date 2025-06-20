cefaf16137f6e09a9e141b1938dc32d963b814e3
nara <parknara0213@gmail.com>
2025-06-20 16:22:58 +0900

    feat: 컴파운드 컴포넌트 패넌을 위한 폴더 구조 생성
    Refs: #17

---
+++ src/shared/ui/Modal/index.ts
@@ -0,0 +1,17 @@
+import {} from "./components";
+
+import {} from "./context";
+
+import {} from "./hooks";
+
+import {} from "./utils";
+
+import type {} from "./types";
+
+export // components
+// context
+// hooks
+// utils
+ {};
+
+export type {};
