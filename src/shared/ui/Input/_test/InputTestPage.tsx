403f09bef71b0c6986ab9fff0014f7c8dc312a20
nara <parknara0213@gmail.com>
2025-06-23 09:53:30 +0900

    feat: InputTestPage 구성
    Refs: #23

--- src/shared/ui/Input/_test/InputTestPage.tsx
+++ src/shared/ui/Input/_test/InputTestPage.tsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import Input from "../components";
 import styles from "./InputTestPage.module.css";
 import { joinClassNames } from "@shared/utils";
+import { colors } from "@shared/data";
+import FocusTrap from "@shared/ui/FocusTrap";
 
 interface InputTestPageProps {}
 
 const InputTestPage = ({}: InputTestPageProps) => {
   const classNames = joinClassNames([styles["input__test__page"]]);
 
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState("테슷흐");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -17,25 +19,50 @@ const InputTestPage = ({}: InputTestPageProps) => {
   };
 
   return (
-    <div className={classNames}>
-      <Input
-        field="test"
-        value={value}
-        onChange={handleChange}
-        label="테스트"
-        maxLength={250}
-        // disabled
-        color="green"
-      >
-        <Input.Main>
-          <Input.Header />
-          <Input.Field>
-            <Input.Text />
-          </Input.Field>
-        </Input.Main>
-        <Input.Extra>추가</Input.Extra>
-      </Input>
-    </div>
+    <FocusTrap>
+      <div className={classNames}>
+        {colors.map((color) => {
+          ("");
+          return (
+            <Input
+              field="test"
+              value={value}
+              onChange={handleChange}
+              label="테스트"
+              maxLength={250}
+              // disabled
+              color={color}
+              isValid
+            >
+              <Input.Main>
+                <Input.Header />
+                <Input.Field>
+                  <Input.Text />
+                </Input.Field>
+              </Input.Main>
+              <Input.Extra>추가</Input.Extra>
+            </Input>
+          );
+        })}
+        <Input
+          field="test"
+          value={value}
+          onChange={handleChange}
+          label="테스트"
+          maxLength={250}
+          disabled
+          isValid
+        >
+          <Input.Main>
+            <Input.Header />
+            <Input.Field>
+              <Input.Text />
+            </Input.Field>
+          </Input.Main>
+          <Input.Extra>추가</Input.Extra>
+        </Input>
+      </div>
+    </FocusTrap>
   );
 };
 
