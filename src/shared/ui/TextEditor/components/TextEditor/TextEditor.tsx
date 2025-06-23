f60ececfd47be17387818c364fd057f574afdbe4
nara <parknara0213@gmail.com>
2025-06-23 22:41:33 +0900

    feat: contentEditable 안에 children이 있는 경우 에러 해결
    Refs: #25

--- src/shared/ui/TextEditor/components/TextEditor/TextEditor.tsx
+++ src/shared/ui/TextEditor/components/TextEditor/TextEditor.tsx
@@ -10,7 +10,11 @@ const TextEditor = ({ className, disabled = false }: TextEditorProps) => {
   const classNames = joinClassNames([styles["text__editor"], className]);
 
   return (
-    <div className={classNames} contentEditable={!disabled}>
+    <div
+      className={classNames}
+      contentEditable={!disabled}
+      suppressContentEditableWarning
+    >
       <div className={styles["line"]} data-offset="0-0">
         <span className={styles["segment"]} data-offset="0-0">
           <br data-text="true" />
