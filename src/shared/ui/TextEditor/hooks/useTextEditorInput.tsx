bf1e8058d4877638e4f1390de5ba744d242b68aa
nara <parknara0213@gmail.com>
2025-07-02 22:21:33 +0900

    fix: 한글 입력 시 커서의 위치에 대한 업데이트가 진행되지 않는 문제 해결

--- src/shared/ui/TextEditor/hooks/useTextEditorInput.tsx
+++ src/shared/ui/TextEditor/hooks/useTextEditorInput.tsx
@@ -12,6 +12,7 @@ interface HandleInputProps {
   e: React.FormEvent<HTMLDivElement>;
   setInnerHTML: React.Dispatch<React.SetStateAction<string>>;
   setCaretPosition: React.Dispatch<React.SetStateAction<ICaretPosition>>;
+  isComposing: boolean;
 }
 
 const useTextEditorInput = () => {
@@ -21,10 +22,15 @@ const useTextEditorInput = () => {
     e,
     setInnerHTML,
     setCaretPosition,
+    isComposing,
   }: HandleInputProps) => {
     const textEditor = e.currentTarget;
 
+    if (isComposing) return;
+
     let { caretPos, row: curRow, col: curCol } = getCaretPosition();
+
+    console.log("변경 전 커서의 위치", caretPos, curRow, curCol);
 
     const lines = extractLines(textEditor);
 
@@ -39,16 +45,10 @@ const useTextEditorInput = () => {
 
       // 현재 줄이 커서가 위치한 줄인 경우
       if (row === curRow) {
-        // 현재 커서가 위치한 segment
-        const curSegment = segments[curCol];
-
-        // 해당 segment의 텍스트
-        const curText = curSegment.text;
-
         // 커서 위치가 현재 segment의 텍스트 길이보다 클 경우
-        if (curText.length < caretPos) {
+        while (segments[curCol].text.length < caretPos) {
           // 커서 위치에서 현재 텍스트 길이를 빼서 남은 caret 위치를 갱신
-          caretPos -= curText.length;
+          caretPos -= segments[curCol].text.length;
           // 다음 segment로 커서 위치를 이동
           curCol += 1;
         }
@@ -62,6 +62,8 @@ const useTextEditorInput = () => {
 
       linesHTML += createLine(segmentHTML, row);
     }
+
+    console.log("변경된 커서 위치", caretPos, curRow, curCol);
 
     setInnerHTML(linesHTML);
     setCaretPosition({ caretPos, row: curRow, col: curCol });
