7dd0c288d1121717632b752678bd3b4ef429cc8d
nara <parknara0213@gmail.com>
2025-07-08 20:27:58 +0900

    feat: textEditor 상태에 isOpen, selectedIndex 필드 추가 및 리듀서 업데이트
    Refs: #26

--- src/shared/types/slices.ts
+++ src/shared/types/slices.ts
@@ -3,6 +3,8 @@ import type { ICaretPosition } from "@shared/ui/TextEditor";
 interface ITextEditor {
   innerHTML: string;
   caretPosition: ICaretPosition;
+  isOpen: boolean;
+  selectedIndex: number;
 }
 
 export type { ITextEditor };
